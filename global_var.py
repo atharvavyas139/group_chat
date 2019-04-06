03a75816a2d9718e59d32ad02e71e4831f2cba42
amanpurwar <amanpurwar35@gmail.com>
2019-04-06 09:48:18 +0530

    updates added

---
+++ global_var.py
@@ -0,0 +1,13 @@
+######### define the global variables/constants here 
+
+######### Message Type 
+JOIN = 1
+REPLY_JOIN = 2
+HELLO = 3
+REPLY_HELLO = 4
+LEAVE = 5
+TEXT_MSG = 6
+ACK = 7
+UPDATE = 8 # Crash recovery 
+
+
