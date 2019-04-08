c13fdd7cd0b30ca248ca9047fd5490a7f5d0fab7
atharvavyas139 <atharvavyas139@gmail.com>
2019-04-08 22:20:27 +0530

    added priority queue for our use

--- user_variables.py
+++ user_variables.py
@@ -33,5 +33,49 @@ self_ip = '127.0.0.1'
 joining_port = 50111 #fixed for the application across all the users 
 sending_port = 50112
 receiving_port = 50113
-join_complete = False 
-# message_queue = for cbcast 
+join_complete = False
+priority_queue= queue.PriorityQueue() 
+# message_queue = for cbcast
+
+########################priority queue #######################
+class Mytimestamp(object): 
+	def __init__(self, time_stamp, time_received, message): 
+		self.timestamp = time_stamp
+		self.time_received = time_received
+		self.message = message
+
+	def __str__(self):
+		return "({0})".format(self.message)
+
+	def __lt__(self, other):
+		b = True
+		for i in range(100):
+			if self.timestamp[i] <= other.timestamp[i]:
+				b = b and True
+			else:
+				b = b and False
+		if b: # defintely less than
+			return True
+		
+		b = True
+		for i in range(100):
+			if self.timestamp[i] >= other.timestamp[i]:
+				b = b and True
+			else:
+				b = b and False
+		if b: # definitely greater 
+			return False
+		#if concurrent
+		return self.time_received < other.time_received
+"""an example of usage
+timestamp1 = [0 for x in range(100)]
+timestamp1[1] = 1
+timestamp1[3] = 4
+rt1 = time.time()
+msg1 = "message1"
+priority_queue.put(Mytimestamp(timestamp1, rt1, msg1))
+while not priority_queue.empty():
+	myt = priority_queue.get()
+	print 'Processing level:', myt
+"""
+
