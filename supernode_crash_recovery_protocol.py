9fa6a3a59683c4ef31ba974b26e89a7c61e0d4ca
basher666 <swastik.haldar@gmail.com>
2019-04-18 13:58:19 +0530

    fixed metadata not receiving bug from replicated supernodes

--- supernode_crash_recovery_protocol.py
+++ supernode_crash_recovery_protocol.py
@@ -8,7 +8,9 @@ from threading import Lock
 import supernode_variables
 import supernode_joining_protocol
 import leaving_protocol
+import struct
 import time
+import select
 
 def startup(port_no):
 	print("starting supernode")
@@ -16,7 +18,7 @@ def startup(port_no):
 		try:
 			if (ip == supernode_variables.self_ip):
 				continue
-			print("sending update requests to supernode : ",ip)
+			print("sending update requests to supernode : ",ip,supernode_variables.recovery_port)
 			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 			s.connect((ip,supernode_variables.recovery_port))
 			msg = {}
@@ -26,9 +28,9 @@ def startup(port_no):
 			print("update request sent")
 			update_vars(port_no)
 			print("updated metadata")
-		except:
+		except Exception,e:
 			s.close()
-			print("exception in startup")
+			print("exception in startup : ",e)
 			pass
 
 
@@ -52,53 +54,74 @@ def startup(port_no):
 def update_vars(port_no):
 	print("udpating vars")
 	try:
+		
 		receive_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		receive_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 		receive_socket.bind(('', port_no))
-		receive_socket.setblocking(1)              #go back to blocking mode
 		
-		## timeout set to 1 sec
+		receive_socket.setblocking(1)              #go back to blocking mode
+		# receive_socket.setblocking(0)
+		print("setting timeout")
+		# timeout set to 1 sec
 		tv = struct.pack("ll", 1, 0)
-		receive_socket.setsockopt(socket.SOL_SOCKET, socket.SO_SNDTIMEO, tv)
+		receive_socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVTIMEO, tv)
 		receive_socket.listen(1)
 
+		# receive_socket.settimeout(1.0)
+		# receive_socket.listen(1)
+
+		print("accepting connection")
 		conn, addr = receive_socket.accept()
-		
+		print("accepted connection")
 		data = conn.recv(4096)
-		data_received = pickle.loads(data)
-		receive_socket.close()
+		
+
+
 		print("received metadata from supernode : ",addr)
+		data_received = pickle.loads(data)
 		supernode_variables.mutex.acquire()
 
 		supernode_variables.index_array = data_received['index_array']
 		supernode_variables.ip_to_username = data_received['ip_to_username']
 		supernode_variables.ip_to_index_map = data_received['ip_to_index_map']
-
+	except Exception,e:
+		print("problem in update_vars :",e)
 	finally:
+		conn.close()
+		receive_socket.close()
 		supernode_variables.mutex.release()
 
 def update_reply(port_no):
 	print("listening for update requests")
 	try:
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-		s.bind(('',port_no))
-		s.listen(102)
 		while True:
-			conn, addr = s.accept()
-			data = conn.recv(4096)
+			receive_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			receive_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+			receive_socket.bind(('',port_no))
+			receive_socket.listen(102)
 
+			conn, addr = receive_socket.accept()
+			data = conn.recv(4096)
+			conn.close()
+			receive_socket.close()
 			msg = pickle.loads(data)
+
 			print("sending metadata to : ",addr)
 			if (msg['msg_type'] == supernode_variables.UPDATE):
-
 				supernode_variables.mutex.acquire()
 				data_send = {}
 				data_send['index_array'] = supernode_variables.index_array
 				data_send['ip_to_username'] = supernode_variables.ip_to_username
 				data_send['ip_to_index_map'] = supernode_variables.ip_to_index_map
-
 				supernode_variables.mutex.release()
-			conn.close()
-	finally:
-		s.close()
+				try:
+					send_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+					send_socket.connect((addr[0],port_no))
+					send_socket.send(pickle.dumps(data_send) )
+					send_socket.close()
+				except:
+					print("problem in sending metadata")
+					send_socket.close()
+	except Exception,e:
+		print("problem in update_reply : ",e)
+		receive_socket.close()
